8ff6e637793305f7e00d0bf40dc9ac8a4aae6b53
Laura <laura@usc-securewireless-student-new235.usc.edu>
2019-09-23 13:10:01 -0700

    changed variable names

--- parameters.m
+++ parameters.m
@@ -3,7 +3,7 @@
 clear;clc;
 
 % learning rate (between 0 and 1)
-alpha_p = 0.00001;
+alpha_p = 0.01;
 
 % inverse temperature
 beta_p = 0.00001;
@@ -15,7 +15,7 @@ gamma_p = 0.99; % or greater
 % epsilon_p = 0.01; 
 
 % max iteration
-max_itr = 10000;
+max_itr = 1000;
 
 % max interation within an episode
 max_ep_itr = 100;
--- softmax.m
+++ softmax.m
@@ -11,13 +11,13 @@ Q_arr = Q(state,:);
 Q_tot = exp(beta_p * Q_arr(1)) + exp(beta_p * Q_arr(2)) + exp(beta_p * Q_arr(3))+ exp(beta_p * Q_arr(4));
 
 % softmax
-softmax1 = exp(beta_p * Q_arr(1))/Q_tot;
-softmax2 = exp(beta_p * Q_arr(2))/Q_tot;
-softmax3 = exp(beta_p * Q_arr(3))/Q_tot;
-softmax4 = exp(beta_p * Q_arr(4))/Q_tot;
+prob1 = exp(beta_p * Q_arr(1))/Q_tot;
+prob2 = exp(beta_p * Q_arr(2))/Q_tot;
+prob3 = exp(beta_p * Q_arr(3))/Q_tot;
+prob4 = exp(beta_p * Q_arr(4))/Q_tot;
 
 % if all softmaxes are equal choose randomly
-if ( softmax1 == softmax2 && softmax2 == softmax3 && softmax3 == softmax4 )
+if ( prob1 == prob2 && prob2 == prob3 && prob3 == prob4 )
     check1 = 0;
     while ( check1 == 0 )
         randnum = rand;
@@ -41,16 +41,16 @@ else
     check2 = 0;
     while ( check2 == 0 )
         randnum = rand;
-        if ( randnum <= softmax1 && action_arr(1) ~= 0)
+        if ( randnum <= prob1 && action_arr(1) ~= 0)
             action = 1;
             check2 = 1;
-        elseif ( randnum <= softmax1 + softmax2 && action_arr(2) ~= 0)
+        elseif ( randnum <= prob1 + prob2 && action_arr(2) ~= 0)
             action = 2;
             check2 = 1;
-        elseif ( randnum <= softmax1 + softmax2 + softmax3 && action_arr(3) ~= 0)
+        elseif ( randnum <= prob1 + prob2 + prob3 && action_arr(3) ~= 0)
             action = 3;
             check2 = 1;
-        elseif ( randnum <= softmax1 + softmax2 + softmax3 + softmax4 && action_arr(4) ~= 0)
+        elseif ( randnum <= prob1 + prob2 + prob3 + prob4 && action_arr(4) ~= 0)
             action = 4;
             check2 = 1;
         end
